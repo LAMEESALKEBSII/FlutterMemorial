ff582396bc23727f3b554d422c7a540a65b58de3
LAMEESALKEBSII <163675003+LAMEESALKEBSII@users.noreply.github.com>
2025-07-23 13:46:01 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "01bf19e48b1d55995cbbdfac790e4e55
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8c1d9a53402a3fd3c67f4c11445df90c",
+"flutter_bootstrap.js": "39b1171c9296f6ab4949b915f4aa64d0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
